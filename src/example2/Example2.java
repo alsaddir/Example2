51b41556ddc967a65898b15ace250f3786a687ba
alsaddir <alsaddir@sheridancollege.ca>
2019-05-26 23:36:14 -0400

    New code for exaample2

---
+++ src/example2/Example2.java
@@ -0,0 +1,30 @@
+package example2;
+/**
+ *
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+public class Example2
+{
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+//      Gender person = Gender.FEMEL;
+//      System.out.println(person);
+//      Gender person2 = Gender.MELE;
+//      System.out.println(person == person2);
+//      for (int i = 0; i < 400; i++) {
+//         Random rnd = new Random();
+//         int random = rnd.nextInt(100);
+//         System.out.println(random);
+
+      //  }
+
+
+      int[][] array = new int[5][];
+      System.out.println(array[0]);
+
+
+   }
+}
---
+++ src/example2/Gender.java
@@ -0,0 +1,11 @@
+package example2;
+
+/**
+ * @Rich Smith at ZenOfProgramming.com
+ */
+
+public enum Gender
+{
+   FEMEL, MELE
+
+}
