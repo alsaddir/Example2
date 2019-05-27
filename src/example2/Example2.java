e7f49dca86da88081b625a88a3bcac301d3eac42
alsaddir <alsaddir@sheridancollege.ca>
2019-05-27 00:20:02 -0400

    Add two linnes

--- src/example2/Example2.java
+++ src/example2/Example2.java
@@ -12,19 +12,19 @@ public class Example2
    {
       Gender person = Gender.FEMEL;
       System.out.println(person);
-//      Gender person2 = Gender.MELE;
-//      System.out.println(person == person2);
-//      for (int i = 0; i < 400; i++) {
-//         Random rnd = new Random();
+      Gender person2 = Gender.MELE;
+      System.out.println(person == person2);
+      for (int i = 0; i < 400; i++) {
+         Random rnd = new Random();
 //         int random = rnd.nextInt(100);
 //         System.out.println(random);
 
-      //  }
+         //  }
 
 
-      int[][] array = new int[5][];
-      System.out.println(array[0]);
+         int[][] array = new int[5][];
+         System.out.println(array[0]);
 
 
+      }
    }
-}
